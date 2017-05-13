06a31289468ea4ec8467188263917cc546b161f0
logonrm <logonrm@L2704MICRO13.fiap.com.br>
2017-05-13 17:01:44 -0300

    .

--- AmigoSecreto/src/br/fiap/t28scj/javaWeb/action/LoginAction.java
+++ AmigoSecreto/src/br/fiap/t28scj/javaWeb/action/LoginAction.java
@@ -1,26 +1,70 @@
 package br.fiap.t28scj.javaWeb.action;
 
-import org.apache.struts2.convention.annotation.Action;
+import java.util.Map;
+
 import org.apache.struts2.convention.annotation.Result;
 import org.apache.struts2.convention.annotation.Results;
+import org.apache.struts2.dispatcher.SessionMap;
+import org.apache.struts2.interceptor.SessionAware;
 
 import com.opensymphony.xwork2.ActionSupport;
-
-import br.fiap.t28scj.javaWeb.dao.GenericDao;
-import br.fiap.t28scj.javaWeb.entity.Usuario;
 
 @Results({ 
 	@Result(name = "ok", location = "/menu.jsp"), 
 	@Result(name = "erro", location = "/login.jsp"),
 	@Result(name = "cadastro", location = "/cadUsuario.jsp")
 })
-public class LoginAction extends ActionSupport {
-
-	private static final long serialVersionUID = 1L;
-
-	@Action(value = "/login")
-	public String listar() {
-		GenericDao<Usuario> dao = new GenericDao<Usuario>(Usuario.class);
-		return "ok";
+public class LoginAction extends ActionSupport implements SessionAware{  
+	private String name,password;  
+	private SessionMap<String,Object> sessionMap;  
+	
+	@Override  
+	public void setSession(Map<String, Object> map) {  
+	    sessionMap=(SessionMap)map;  
+	}  
+	  
+	public String execute(){  
+		System.out.println(sessionMap.values());
+		if(password.equals("admin")){  
+		    sessionMap.put("login","true");  
+		    sessionMap.put("name",name);  
+		      
+		    return "ok";  
+		}  
+		else{  
+		    return "erro";  
+		}  
+	}  
+	  
+	public String logout(){  
+	    if(sessionMap!=null){  
+	        sessionMap.invalidate();  
+	    }  
+	    return "erro";  
 	}
-}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public SessionMap<String, Object> getSessionMap() {
+		return sessionMap;
+	}
+
+	public void setSessionMap(SessionMap<String, Object> sessionMap) {
+		this.sessionMap = sessionMap;
+	}  
+	  
+}  
